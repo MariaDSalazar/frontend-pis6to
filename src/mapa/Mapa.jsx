75035132996221aefde400606637dbc737f65b69
patricio <patobetan8@gmail.com>
2024-02-08 18:51:51 -0500

    cambios

--- src/mapa/Mapa.jsx
+++ src/mapa/Mapa.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import "leaflet/dist/leaflet.css";
 import "./Mapa.css";
 
 const Mapa = () => {
+  const [dispositivos, setDispositivos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://computacion.unl.edu.ec/uv/api/activos')
+      .then(response => response.json())
+      .then(data => {
+        if (data && data.dispositivos) {
+          setDispositivos(data.dispositivos);
+        }
+      })
+      .catch(error => {
+        console.error('Error al obtener los dispositivos:', error);
+      });
+  }, []);
+
   const customIcon = new L.Icon({
     iconUrl: '/icons/puntero.ico',
     iconSize: [64, 64], // Ajusta el tamaño según tus necesidades
@@ -17,18 +32,17 @@ const Mapa = () => {
       <MapContainer center={[-4.030256, -79.199463]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-          
         />
-        <Marker position={[-4.030256, -79.199463]} icon={customIcon}>
-          <Popup position={[-4.030256, -79.199463]}>
-            Facultad de la salud humana
-            <br />
-            8.45 uv
-          </Popup>
-        </Marker>
-        
+        {dispositivos.map(dispositivo => (
+          <Marker key={dispositivo.external_id} position={[dispositivo.latitud, dispositivo.longitud]} icon={customIcon}>
+            <Popup position={[dispositivo.latitud, dispositivo.longitud]}>
+              {dispositivo.nombre}
+              <br />
+              {dispositivo.uv ? `${dispositivo.uv} uv` : 'Información no disponible'}
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
-      
     </div>
   );
 }
